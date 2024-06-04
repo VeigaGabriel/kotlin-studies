a4b746175f78de8c490f3b47545c7299bd86eeab
VeigaGabriel <gabrielvalmeidaa@gmail.com>
2024-06-04 20:16:06 -0300

    starting project

---
+++ src/main/kotlin/com/betrybe/aula01/App.kt
@@ -0,0 +1,9 @@
+package com.betrybe.aula01
+
+fun main() {
+    print("Digite seu nome: ")
+    val nome: String = readln()
+    print("Olá $nome!")
+
+
+}
---
+++ src/main/kotlin/com/betrybe/aula01/Ex01.kt
@@ -0,0 +1,10 @@
+package com.betrybe.aula01
+
+/*
+Escrever um programa que receba três números inteiros,
+calcule e mostre a média aritimética simples entre os números
+*/
+
+fun main() {
+
+}
