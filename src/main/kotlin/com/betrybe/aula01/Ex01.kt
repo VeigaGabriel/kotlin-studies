107d45ce03c2cdd98b5741fd02dc3b25f130849a
VeigaGabriel <gabrielvalmeidaa@gmail.com>
2024-06-04 21:17:10 -0300

    primeiro dia de aula

--- src/main/kotlin/com/betrybe/aula01/Ex01.kt
+++ src/main/kotlin/com/betrybe/aula01/Ex01.kt
@@ -7,4 +7,21 @@ calcule e mostre a média aritimética simples entre os números
 
 fun main() {
 
+    val numberOrder: Array<String> = arrayOf("primeiro", "segundo", "terceiro", "quarto")
+    var numberArray: Array<Double> = arrayOf()
+
+    for (item in numberOrder) {
+        print("Digite o $item número:")
+        numberArray += readln().toDouble()
+    }
+
+    println("Lista de números: ${numberArray.joinToString()}")
+    println("Média: ${calcularMedia(numberArray)}")
 }
+
+//fun calcularMedia(numbers: Array<Int>): Int {
+//    val sum: Int = numbers.reduce { acc, num -> acc + num }
+//    return sum / numbers.size
+//}
+
+fun calcularMedia(numbers: Array<Double>) = numbers.reduce { acc, num -> acc + num } / numbers.size
